df9ac2db6e7e27bd71486af35a7d8cef1067e717
unknown <official.jaskaran13@gmail.com>
2026-04-25 20:10:11 +0530

    sessionidchanges

--- src/utils.ts
+++ src/utils.ts
@@ -9,7 +9,7 @@ export function generateVisitorId(): string {
 }
 
 export function generateSessionId(): string {
-  return "sxxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
     const r = Math.random() * 16 | 0;
     const v = c === "x" ? r : r & 3 | 8;
     return v.toString(16);
